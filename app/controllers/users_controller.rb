c4f514c95db1c314f66f232e93224c28ffee4907
arssy <hafidzala@gmail.com>
2020-01-12 11:55:29 +0700

    create user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
+        flash[:success] = "User data has been updated"
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
